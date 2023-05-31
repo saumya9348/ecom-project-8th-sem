2112244b0199b3434723dc650d26bc26e98a916a
Saumya Pani <saumyapani@Saumyas-MacBook-Pro.local>
2023-06-01 04:04:15 +0530

    build code added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import paymentRoutes from "./routes/payment.js"
 import cors from "cors";
-import path from path
+import path from "path"
 import {fileURLToPath} from "url"
 import { requireSignIn } from "./middlewares/authMiddleware.js";
 import {
